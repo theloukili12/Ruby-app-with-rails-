dcd90438a2ddac1912d92d32a9214c00cf75c56e
theloukili12 <loukilisoufianee@gmail.com>
2021-01-06 23:28:17 +0100

    simple-app

---
+++ test/system/tasks_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class Project::TasksTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit project_tasks_url
+  #
+  #   assert_selector "h1", text: "Project::Task"
+  # end
+end
